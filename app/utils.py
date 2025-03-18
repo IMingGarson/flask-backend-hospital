6fb5aedbd86ba1ca3732e03b7272c7a3cb3d5aee
IMing <hobnob848484@gmail.com>
2025-03-18 16:19:49 +0800

    fix: rollback

--- app/utils.py
+++ app/utils.py
@@ -3,33 +3,34 @@ from datetime import datetime
 from itsdangerous import URLSafeTimedSerializer
 from email_validator import validate_email, EmailNotValidError
 import bcrypt
-from exponent_server_sdk import (
-    PushClient,
-    PushMessage,
-)
+import requests
+# from exponent_server_sdk import (
+#     PushClient,
+#     PushMessage,
+# )
 
 def send_push_notification(push_token, title, body):
-    client = PushClient()
-    response = client.publish(PushMessage(to=push_token, title=title, body=body, sound="default", channel_id="default"))
-    response.validate_response()
-    return 200, {"status": "ok"}
+    # client = PushClient()
+    # response = client.publish(PushMessage(to=push_token, title=title, body=body, sound="default", channel_id="default"))
+    # response.validate_response()
+    # return 200, {"status": "ok"}
 
-    # message = {
-    #     "to": push_token,
-    #     "sound": "default",
-    #     "title": title,
-    #     "body": body,
-    # }
-    # response = requests.post(
-    #     "https://exp.host/--/api/v2/push/send",
-    #     headers={
-    #         "Accept": "application/json",
-    #         "Accept-Encoding": "gzip, deflate",
-    #         "Content-Type": "application/json",
-    #     },
-    #     json=message,
-    # )
-    # return response.status_code, response.json()
+    message = {
+        "to": push_token,
+        "sound": "default",
+        "title": title,
+        "body": body,
+    }
+    response = requests.post(
+        "https://exp.host/--/api/v2/push/send",
+        headers={
+            "Accept": "application/json",
+            "Accept-Encoding": "gzip, deflate",
+            "Content-Type": "application/json",
+        },
+        json=message,
+    )
+    return response.status_code, response.json()
 
 def validate_date(date_str):
     try:
